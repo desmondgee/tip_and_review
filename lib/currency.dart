150c9f8a027f4c6238f607bb85a005d0cd263385
desmond <desmondgee00@gmail.com>
2021-04-24 23:10:48 -0700

    removed unused code and moved currency functions to its own file.

---
+++ lib/currency.dart
@@ -0,0 +1,31 @@
+class Currency {
+  // Converts a formatted dollar value such as `$12.50` into a cents integer as `1250`
+  // Trims trailing digits to become less than `$10,000`. i.e. `$1,234,567.89` becomes `123456` cents. This is to stop addition number input when value is too high already for input box.
+  static int parseCents(String formattedDollars) {
+    String strCents = formattedDollars.splitMapJoin((RegExp(r'[0-9]')),
+        onNonMatch: (n) => '');
+
+    int cents = strCents == '' ? 0 : int.parse(strCents);
+    if (cents > 999999) {
+      cents = (cents * 0.1).floor();
+    }
+    return cents;
+  }
+
+  // TODO: Add comma separators.
+  static String formatCents(int cents) {
+    double dollars = cents / 100;
+    return '\$' + dollars.toStringAsFixed(2);
+  }
+
+  static String reformatDollars(String formattedDollars) {
+    return formatCents(parseCents(formattedDollars));
+  }
+
+  // Converts a formatted percent such as `12.5%` into a multipliable double such as `0.125`
+  static double parsePercent(String formattedPercent) {
+    String numbers =
+        RegExp(r'[0-9]+\.?[0-9]*').stringMatch(formattedPercent) ?? '0';
+    return double.parse(numbers) * 0.01;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,42 +4,11 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 import 'package:flutter/services.dart';
-import 'package:numberpicker/numberpicker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'currency.dart';
 
 void main() => runApp(MyApp());
-
-// Converts a formatted dollar value such as `$12.50` into a cents integer as `1250`
-// Trims trailing digits to become less than `$10,000`. i.e. `$1,234,567.89` becomes `123456` cents. This is to stop addition number input when value is too high already for input box.
-int parseCents(String formattedDollars) {
-  String strCents =
-      formattedDollars.splitMapJoin((RegExp(r'[0-9]')), onNonMatch: (n) => '');
-
-  int cents = strCents == '' ? 0 : int.parse(strCents);
-  if (cents > 999999) {
-    cents = (cents * 0.1).floor();
-  }
-  return cents;
-}
-
-// TODO: Add comma separators.
-String formatCents(int cents) {
-  double dollars = cents / 100;
-  return '\$' + dollars.toStringAsFixed(2);
-}
-
-String reformatDollars(String formattedDollars) {
-  return formatCents(parseCents(formattedDollars));
-}
-
-// Converts a formatted percent such as `12.5%` into a multipliable double such as `0.125`
-double parsePercent(String formattedPercent) {
-  String numbers =
-      RegExp(r'[0-9]+\.?[0-9]*').stringMatch(formattedPercent) ?? '0';
-  return double.parse(numbers) * 0.01;
-}
 
 class MyApp extends StatelessWidget {
   @override
@@ -92,6 +61,7 @@ class _TipMainState extends State<TipMain> {
   );
 
   final tipPercents = <String>[
+    "0%",
     "5%",
     "10%",
     "12%",
@@ -105,17 +75,13 @@ class _TipMainState extends State<TipMain> {
     "30%",
   ];
 
-  int _tipValue = 15;
-
   var inputTotalController = TextEditingController();
 
-  int selectedIndex = 5;
   FixedExtentScrollController inputTipController;
 
   @override
   void initState() {
-    inputTipController =
-        FixedExtentScrollController(initialItem: selectedIndex);
+    inputTipController = FixedExtentScrollController(initialItem: 5);
     super.initState();
     _loadPrefs();
   }
@@ -128,7 +94,6 @@ class _TipMainState extends State<TipMain> {
     int index = prefs.getInt('tipIndex');
     if (index != null) {
       setState(() {
-        selectedIndex = index;
         // inputTipController.jumpToItem(index);
       });
     }
@@ -141,7 +106,6 @@ class _TipMainState extends State<TipMain> {
     setState(() {
       int index = inputTipController.selectedItem;
       prefs.setInt('tipIndex', index);
-      selectedIndex = index;
     });
   }
 
@@ -155,11 +119,13 @@ class _TipMainState extends State<TipMain> {
   }
 
   Widget _youPayBlock() {
-    int inputCents = parseCents(inputTotalController.text);
+    int inputCents = Currency.parseCents(inputTotalController.text);
     // can't access selectedItem until controller is attached to view
     // String formattedTipPercent = tipPercents[inputTipController.selectedItem];
-    String formattedTipPercent = tipPercents[selectedIndex];
-    double tipFraction = parsePercent(formattedTipPercent);
+    int index =
+        inputTipController.hasClients ? inputTipController.selectedItem : 5;
+    String formattedTipPercent = tipPercents[index];
+    double tipFraction = Currency.parsePercent(formattedTipPercent);
     int tipCents = (inputCents * tipFraction).floor();
     int totalCents = inputCents + tipCents;
 
@@ -187,7 +153,7 @@ class _TipMainState extends State<TipMain> {
                             SizedBox(
                                 width: 140,
                                 child: Text(
-                                  formatCents(tipCents),
+                                  Currency.formatCents(tipCents),
                                   style: textStyle,
                                   textAlign: TextAlign.right,
                                 ))
@@ -205,7 +171,7 @@ class _TipMainState extends State<TipMain> {
                             SizedBox(
                                 width: 140,
                                 child: Text(
-                                  formatCents(totalCents),
+                                  Currency.formatCents(totalCents),
                                   style: textStyle,
                                   textAlign: TextAlign.right,
                                 ))
@@ -230,28 +196,6 @@ class _TipMainState extends State<TipMain> {
       tiles: tipTiles,
     ).toList();
 
-    final scrollerVersion1 = SizedBox(
-        width: 100,
-        height: 100,
-        child: ListWheelScrollView(
-          itemExtent: 44,
-          children: dividedTipTiles,
-          useMagnifier: true,
-          magnification: 1,
-          diameterRatio: 4,
-        ));
-
-    final scrollerVersion2 = SizedBox(
-        width: 100,
-        height: 200,
-        child: NumberPicker(
-            value: _tipValue,
-            minValue: 0,
-            maxValue: 100,
-            onChanged: (value) => setState(() => _tipValue = value)));
-
-    // inputTipController =
-    //     FixedExtentScrollController(initialItem: selectedIndex);
     final scrollerVersion3 = SizedBox(
         width: 100,
         height: 150,
@@ -288,8 +232,8 @@ class _TipMainState extends State<TipMain> {
                                     FilteringTextInputFormatter.digitsOnly,
                                     TextInputFormatter.withFunction(
                                         (oldValue, newValue) {
-                                      String text =
-                                          reformatDollars(newValue.text);
+                                      String text = Currency.reformatDollars(
+                                          newValue.text);
                                       return TextEditingValue(
                                           text: text,
                                           selection: TextSelection.collapsed(
@@ -308,108 +252,5 @@ class _TipMainState extends State<TipMain> {
                         ])
                       ]),
                 ]))));
-  }
-}
-
-MaterialApp exampleApp() {
-  return MaterialApp(
-    title: 'Startup Name Generator',
-    theme: ThemeData(
-      primaryColor: Colors.white,
-    ),
-    home: RandomWords(),
-  );
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
-  final _biggerFont = TextStyle(fontSize: 18.0);
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final tiles = _saved.map(
-            (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
-            ),
-            body: ListWheelScrollView(
-              itemExtent: 84,
-              children: divided,
-              useMagnifier: true,
-              magnification: 1,
-              diameterRatio: 1.5,
-            ),
-          );
-        },
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: [
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider();
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return ListTile(
-        title: Text(pair.asPascalCase, style: _biggerFont),
-        trailing: Icon(
-          alreadySaved ? Icons.favorite : Icons.favorite_border,
-          color: alreadySaved ? Colors.red : null,
-        ),
-        onTap: () {
-          setState(() {
-            if (alreadySaved) {
-              _saved.remove(pair);
-            } else {
-              _saved.add(pair);
-            }
-          });
-        });
   }
 }
